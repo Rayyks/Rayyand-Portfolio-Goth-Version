184380cc5a2627da64a77668c6969a40bd8c03fe
Rayyand <rayydna14@gmail.com>
2023-11-19 19:31:58 +0700

    redeploy

--- components/intro.tsx
+++ components/intro.tsx
@@ -92,7 +92,7 @@ export default function Intro() {
 
         <a
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
-          href="/CV_RAYYAND_ENG.pdf"
+          href="/c.pdf"
           download
         >
           Download CV{" "}
