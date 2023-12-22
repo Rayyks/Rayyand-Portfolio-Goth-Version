a0ea9b3a1766c60eb83247dbbfa40776d779f056
Rayyand <rayydna14@gmail.com>
2023-12-22 14:02:51 +0700

    update laundry mobile github link

--- lib/data.ts
+++ lib/data.ts
@@ -141,7 +141,7 @@ export const projectsData = [
     tags: ["Flutter", "Dart", "MySql", "Laravel"],
     imageUrl: Clean,
     demoUrl: null,
-    codeUrl: "https://github.com/Rayyks/CLEAN.IN ",
+    codeUrl: "https://github.com/Rayyks/CLean-In",
   },
   {
     title: "Qr code attendance",
